1ba808917323b1beca7fd5cf56d4cc5d370e85ec
handekoksal <koksalhande1@gmail.com>
2025-02-05 10:22:26 +0300

    Update Cipher.java

--- Cipher.java
+++ Cipher.java
@@ -7,7 +7,7 @@ public class Cipher
     public static final String ORIGINAL_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
     public static final String CIPHER_ALPHABET = "dfxyhrklvwuasgimnojpqetbcz";
 
-    public String encrypt(String inputString) {
+   public String encrypt(String inputString) {
         
         // output string will be collected in this variable, one char at a time
         String outputString = "";
@@ -15,18 +15,22 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-
+          outputString += replaceChar(inputString.charAt(i), true);        
         }
 
         return outputString;
     }
 
-    public String decrypt(String inputString) {
+   public String decrypt(String inputString) {
         
         // output string will be collected in this variable, one char at a time
         String outputString = "";
-        
-        replaceChar('a',true);
+
+        // for all chars in the input string
+        for (int i = 0; i < inputString.length(); i++)   
+        {
+          outputString += replaceChar(inputString.charAt(i), false);        
+        }
         
         return outputString;
     }
@@ -42,6 +46,7 @@ public class Cipher
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
+                    return CIPHER_ALPHABET.charAt(i);
 
                 }
             }
